4b69702e70317e2b3cc9e3a8eb888d8c6002f142
Jordan Rogus <jordanrogus@gmail.com>
2021-12-01 10:36:11 -0700

    init

---
+++ output-01a.js
@@ -0,0 +1,38 @@
+// Code to read an input file and push it to an array
+import { readFileSync } from "fs";
+var text = readFileSync("./input-01").toString('utf-8');
+var textByLine = text.split("\n")
+
+// Initialize count
+var countIncreases = 0;
+
+/* LOOP THROUGH ARRAY WITH FUNCTION */
+/*
+// Loop through array
+for (var i = 0; i < textByLine.length; i++) {
+  if (typeof (textByLine[i + 1]) !== "undefined") {
+    if (parseInt(textByLine[i + 1]) > parseInt(textByLine[i])) {
+      countIncreases++;
+    }
+  }
+}
+*/
+
+/* LOOP THROUGH ARRAY WITH CONST */
+const hasIncreased = textByLine => {
+  for (let i = 0; i < textByLine.length; i++) {
+    let current = parseInt(textByLine[i]);
+    let next = parseInt(textByLine[i + 1]);
+
+    if (next !== "undefined") {
+      if (next > current) {
+        countIncreases++;
+      }
+    }
+  }
+  return countIncreases;
+}
+
+// Logging to consle
+// To log to console run: node <filename> when your console is in the correct folder
+console.log(hasIncreased(textByLine));
---
+++ output-01b.js
@@ -0,0 +1,32 @@
+// Code to read an input file and push it to an array
+var fs = require("fs");
+var text = fs.readFileSync("./advent-input-01").toString('utf-8');
+var textByLine = text.split("\n")
+
+// Initialize count and new array
+var countIncreases = 0;
+var summedArray = [];
+
+// Loop through array, and create a new array with the laddered sums
+for (var i = 0; i < textByLine.length; i++) {
+  var meas1 = parseInt(textByLine[i]);
+  var meas2 = parseInt(textByLine[i + 1]);
+  var meas3 = parseInt(textByLine[i + 2]);
+  if (typeof (meas1) !== "undefined" || typeof (meas2) !== "undefined" || typeof (meas3) !== "undefined") {
+    var sum = meas1 + meas2 + meas3;
+  }
+  summedArray.push(sum)
+}
+
+// Loop through laddered array and check if the next sum is greater - If so add to count
+for (var i = 0; i < summedArray.length; i++) {
+  if (typeof (summedArray[i + 1]) !== "undefined") {
+    if (parseInt(summedArray[i + 1]) > parseInt(summedArray[i])) {
+      countIncreases++;
+    }
+  }
+}
+
+// Logging to consle
+// To log to console run: node <filename> when your console is in the correct folder
+console.log(countIncreases);
